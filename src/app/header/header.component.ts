aa1d8a0cd4306d08ea0f6473c93159887e503e8c
Shah <workwithme.shah@gmail.com>
2022-12-04 12:21:43 +0530

    cart item

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  item1 = "Keyboard x 1";
+  item2 = "Mouse x 1";
+  item3 = "Monitor x 1";
+  item4 = "Speaker x 1";
+  price1 = 3250;
+  price2 = 840;
+  price3 = 13750;
+  price4 = 5200;
 
+  totalPrice = (): number => {
+    return this.price1 + this.price2 +
+      this.price3 + this.price4
+  }
 }
